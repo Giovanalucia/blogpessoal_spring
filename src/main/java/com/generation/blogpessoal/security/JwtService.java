40fd3fa74460261c23284351e90c86d8d4df84d7
Giovana <giovanalucia835@gmail.com>
2025-07-06 23:36:01 -0300

    :wrench: Ajustes nos componentes Properties

--- src/main/java/com/generation/blogpessoal/security/JwtService.java
+++ src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -19,8 +19,6 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtService {
 
-	public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
-
 	@Value("${jwt.secret}")
 	private String secret;
 	
